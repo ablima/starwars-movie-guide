2d5842932b63bbbd1a8a7300710cc96dae702a3e
Anderson Lima <lander98@volvocars.com>
2026-01-18 21:41:34 +0100

    Implemented filters
    Signed-off-by: Anderson Lima <lander98@volvocars.com>

--- src/pages/MovieList/Filters/filters.tsx
+++ src/pages/MovieList/Filters/filters.tsx
@@ -1,12 +1,34 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Dropdown, DropdownButton, Form, Row } from "react-bootstrap";
 import './filters.css';
 
-const ListFilters = () => {
+interface ListFiltersProps {
+  setSort: (sort: 'episode' | 'year') => void;
+  filter: string;
+  setFilter: (filter: string) => void;
+}
+
+const ListFilters = ({
+  setSort,
+  filter,
+  setFilter
+}: ListFiltersProps) => {
   return (
     <Container fluid className="filters">
       <Row className="filterRow">
-        <Col md={3}>Sort</Col>
-        <Col md={9}>Filter</Col>
+        <Col md={"auto"}>
+          <DropdownButton id="dropdown-basic-button" title="SORT BY..." variant="outline-primary">
+            <Dropdown.Item onClick={() => setSort('episode')}>Episode</Dropdown.Item>
+            <Dropdown.Item onClick={() => setSort('year')}>Year</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+        <Col lg={true}>
+          <Form.Control
+            type="text"
+            value={filter}
+            placeholder="Type to filter..."
+            onChange={(e) => setFilter(e.target.value)}
+          />
+        </Col>
       </Row>
     </Container>
   );
--- src/pages/MovieList/index.tsx
+++ src/pages/MovieList/index.tsx
@@ -3,13 +3,39 @@ import useGetSagaList from "../../hooks/useGetSagaList";
 import ListFilters from "./Filters/filters";
 import List from "./List/list";
 import Details from "./Details/details";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { MovieDetails } from "../../types";
 import './index.css';
 
 const MovieList = () => {
   const { data, loading, error } = useGetSagaList();
+  const [listData, setListData] = useState<MovieDetails[] | null>(null);
   const [selectedMovie, setSelectedMovie] = useState<MovieDetails | null>(null);
+  const [sort, setSort] = useState<'episode' | 'year'>('episode');
+  const [filter, setFilter] = useState<string>('');
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      let filteredData = [...data];
+      if (sort === 'episode') {
+        filteredData.sort((a, b) => a.episode_id - b.episode_id);
+      } else if (sort === 'year') {
+        filteredData.sort((a, b) => {
+          const yearA = parseInt(a.release_date.split('-')[0]);
+          const yearB = parseInt(b.release_date.split('-')[0]);
+          return yearA - yearB;
+        });
+      }
+
+      if (filter.trim() !== '') {
+        filteredData = filteredData.filter(movie =>
+          movie.title.toLowerCase().includes(filter.toLowerCase())
+        );
+      }
+
+      setListData(filteredData);
+    }
+  }, [sort, filter, data]);
 
   if (loading) return <Spinner animation="border" />;
   if (error) return <div>Error: {error}</div>;
@@ -18,13 +44,17 @@ const MovieList = () => {
     <Container fluid>
       <Row className="filterContainer">
         <Col>
-          <ListFilters />
+          <ListFilters
+            setSort={setSort}
+            filter={filter}
+            setFilter={setFilter}
+          />
         </Col>
       </Row>
       <Row>
         <Col className="moviesContainer">
           <List
-            movies={data ?? []}
+            movies={listData ?? []}
             selectedEpisodeId={selectedMovie?.episode_id}
             setSelectedMovie={setSelectedMovie}
           />
